53c1e9784fcb68fae10e010f9d27f4fa9b4c1afb
Ashfaq Mahmud <ash.mse21@gmail.com>
2022-12-18 08:31:31 +0600

    branch-1_for_posting_job_and_fetching_company_data

--- app/src/main/java/com/example/systemprojectadmin/GrowthFragment.java
+++ app/src/main/java/com/example/systemprojectadmin/GrowthFragment.java
@@ -2,63 +2,111 @@ package com.example.systemprojectadmin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GrowthFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class GrowthFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public GrowthFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GrowthFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GrowthFragment newInstance(String param1, String param2) {
-        GrowthFragment fragment = new GrowthFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    LinearLayout layout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_growth, container, false);
+        View view = inflater.inflate(R.layout.fragment_growth, container, false);
+        FirebaseDatabase node =FirebaseDatabase.getInstance();
+        DatabaseReference reference= node.getReference();
+        TextView tname,tmail,ttype,tabout,tloc,tsize;
+
+        layout = view.findViewById(R.id.hiddenlayout);
+        Button edit, enter, close;
+        edit =view.findViewById(R.id.editbutton);
+        enter =view.findViewById(R.id.postb);
+        close =view.findViewById(R.id.closeb);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setVisibility(View.VISIBLE);
+            }
+        });
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        /*tname = view.findViewById(R.id.username);
+        tmail = view.findViewById(R.id.usermail);
+        ttype = view.findViewById(R.id.type);
+        tabout= view.findViewById(R.id.about);
+        tloc= view.findViewById(R.id.companyloc);
+        tsize= view.findViewById(R.id.companysize);
+        //String uid = getArguments().getString("Companykey");
+        reference.child("CompanyDB").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child(uid).child("name").getValue(String.class);
+                String mail = snapshot.child(uid).child("email").getValue(String.class);
+                String size = snapshot.child(uid).child("Size").getValue(String.class);
+                String cat = snapshot.child(uid).child("category").getValue(String.class);
+                String loc = snapshot.child(uid).child("Location").getValue(String.class);
+                String about = snapshot.child(uid).child("about").getValue(String.class);
+
+                *//*val18 = snapshot.child(uid).child("Economical value").child("2018").getValue(Integer.class);
+                val19 = snapshot.child(uid).child("Economical value").child("2019").getValue(Integer.class);
+                val20 = snapshot.child(uid).child("Economical value").child("2020").getValue(Integer.class);
+                val21 = snapshot.child(uid).child("Economical value").child("2021").getValue(Integer.class);
+                val22 = snapshot.child(uid).child("Economical value").child("2022").getValue(Integer.class);*//*
+
+                tname.setText(name);
+                tmail.setText(mail);
+                ttype.setText(cat);
+                tabout.setText(about);
+                tloc.setText(loc);
+                tsize.setText(size);
+
+                GraphView graph = (GraphView) view.findViewById(R.id.graph);
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                        new DataPoint(2018, val18),
+                        new DataPoint(2019, val19),
+                        new DataPoint(2020, val20),
+                        new DataPoint(2021, val21),
+                        new DataPoint(2022, val22)
+                });
+                graph.addSeries(series);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });*/
+
+
+
+
+
+
+
+
+
+        return view;
     }
 }
